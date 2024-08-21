07e90583028c5bba3454debe55293fa2fe85ce49
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-08-21 11:58:37 +0000

    Make changes for deployment to render

---
+++ tinder-clone/build/precache-manifest.9bbedca41a9a53cb15e8e5dac039fbd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fac7e7e75528dc13f2e5e7adf64290f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c7892e38a627a23ad5",
+    "url": "/static/css/main.e5db3807.chunk.css"
+  },
+  {
+    "revision": "0aa17bf6e755f4be2913",
+    "url": "/static/js/2.ff2562fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ff2562fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c7892e38a627a23ad5",
+    "url": "/static/js/main.4070229d.chunk.js"
+  },
+  {
+    "revision": "67b591f0d0936d5c2a28",
+    "url": "/static/js/runtime-main.772c1402.js"
+  }
+]);
